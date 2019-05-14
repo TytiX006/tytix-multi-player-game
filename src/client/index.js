14dba51e457ba0416c1e56aab60dedb6f9944347
Antoine Belliard <tsi.belliard@gmail.com>
2019-05-14 18:51:06 +0200

    Add html to avoid craching

---
+++ src/client/index.js
@@ -0,0 +1,4 @@
+var h = document.createElement("H1");
+var t = document.createTextNode("Yes");
+h.appendChild(t);
+document.body.appendChild(h);
--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname,'./src/client/index.js'),
   output: {
     path: path.resolve(__dirname, './dist/client'),
     filename: 'webpack.client.bundle.js'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
 };
